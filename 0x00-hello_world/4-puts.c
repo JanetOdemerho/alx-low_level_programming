76b835977c702625da08ff8018949005e1e0ea4d
JanetOdemerho <jannyoddie@gmail.com>
2022-09-08 12:22:48 -0700

    Edit task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,10 @@
 
 /**
 * main - to print the string in the 'puts' function
-* return: 0 after printing the function
+* 
+* Description: the main function prints -Programming is like building a 
+* multilingual puzzle-
+* Return: 0 after printing the function
 */
 
 int main(void)
