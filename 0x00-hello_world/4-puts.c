09c119e3d6f894148b8dcb3aae6a69bb013c5453
JanetOdemerho <jannyoddie@gmail.com>
2022-09-08 12:10:38 -0700

    Reads a script that prints a string with the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - to print the string in the 'puts' function
+* return: 0 after printing the function
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
