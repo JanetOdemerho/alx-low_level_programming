e180c3409efade45dc7235b1cf69e29507e98970
JanetOdemerho <jannyoddie@gmail.com>
2022-09-08 13:44:51 -0700

    Write a C program that prints string with a printf function and followed with a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string value in the printf function
+ *
+ * Description: the main function prints the string
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
