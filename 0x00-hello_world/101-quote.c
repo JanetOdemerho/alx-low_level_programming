a2497d7267e1aa9fb537f978438729f42e7bfb6c
JanetOdemerho <jannyoddie@gmail.com>
2022-09-08 17:49:14 -0700

    Task 8 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-
 /**
- * main -print a string
- *
- * Description: prints a string
+ * main - print string
+ * 
+ * Description: print string
  * Return: 1
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", s);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
