c0008c92ee92fc7fcdc09f217e19595697c0ebbd
JanetOdemerho <jannyoddie@gmail.com>
2022-09-09 13:18:27 -0700

    Correction for task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,6 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
@@ -27,6 +26,5 @@ int main(void)
 	{
 		printf("%i is negative\n", n);
 	}
-
 	return (0);
 }
