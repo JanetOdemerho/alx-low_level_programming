ccb76ba79c39bcb660b34fd32e9d02cc0ce3a981
JanetOdemerho <jannyoddie@gmail.com>
2022-09-09 16:57:27 -0700

    The program that prints all single digit numbers of base 10 starting from 0 without the char function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of base 10 without the char function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+		for (n = '0'; n <= 9; n++)
+			putchar(n);
+		putchar("\n");
+	return (0);
+}
