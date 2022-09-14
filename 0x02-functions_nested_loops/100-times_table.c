ed108e4d2fabe7bbed0ce85105fd0ed403f48c67
JanetOdemerho <jannyoddie@gmail.com>
2022-09-13 23:08:47 -0700

    Hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,42 +7,42 @@
  */
 void print_times_table(int n)
 {
-	int l
-	it o
-	int y
-	if (n >= 0 && n <= 15)
-	{
-		for (l = 0; l <= n; l++)
+	int l;
+	int o;
+	int y;
+		if (n >= 0 && n <= 15)
 		{
-			y = l *o;
-			if (o == 0)
+			for (l = 0; l <= n; l++)
 			{
-				_putchar(k + '0');
-			}
-			else if (y < 10 && o != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(y + '0');
-			}
-			else if (y >= 10 && y < 100)
-			{
-				_putchar(',');
-				_putchar(',');
-				_putchar(' ');
-				_putchar((y / 10) + '0');
-				_putchar((y % 10) + '0');
-			}
-			else if (y >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((y / 100) + '0');
-				_putchar(((k / 10) % 10) + '0');
-				_putchar((k % 10) + '0');
+				y = l * o;
+				if (o == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (y < 10 && o != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(y + '0');
+				}
+				else if (y >= 10 && y < 100)
+				{
+					_putchar(',');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((y / 10) + '0');
+					_putchar((y % 10) + '0');
+				}
+				else if (y >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((y / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
 		}
-	}
 }
