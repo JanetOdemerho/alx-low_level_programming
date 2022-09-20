65251ff45926bb448da53cd723b3035bf72e0e8d
JanetOdemerho <jannyoddie@gmail.com>
2022-09-19 19:32:21 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts_half - entry point
@@ -20,9 +21,9 @@ void puts_half(char *str)
 
 	while (*str != '\0')
 	{
-		_putchar(*str);
+		putchar(*str);
 		str++;
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
