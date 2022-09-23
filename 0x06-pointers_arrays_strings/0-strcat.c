ae9622ebac29c7b2b6272e64163b9bc48c320055
JanetOdemerho <jannyoddie@gmail.com>
2022-09-22 17:51:34 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,17 @@
  * Return: destination string
  */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[dest_len])
+	while (dest[index++])
 	{
 		dest_len++;
 	}
-	for (index = 0; src[index] != 0; index++)
+	for (index = 0; src[index]; index++)
 	{
 		dest[dest_len++] = src[index];
 	}
-	dest[dest_len] = '\0';
 	return (dest);
 }
