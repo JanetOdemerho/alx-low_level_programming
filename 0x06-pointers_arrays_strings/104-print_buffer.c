58df16c9af316fa8ef2b6d76590dcb60c9e41c02
JanetOdemerho <jannyoddie@gmail.com>
2022-09-22 22:35:21 -0700

    Task 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - entry point
+ * @c: first parameter
+ * @s: second parameter
+ * @l: third parameter
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int a, i;
+
+	for (a = 0; a < size; a += 10)
+	{
+		printf("%08x: ", a);
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + a) >= size)
+				printf("  ");
+			else
+				printf("%02x", *(b + i + a));
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
+		}
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + a) >= size)
+				break;
+			else if (*(b + i + a) >= 31 &&
+				 *(b + i + a) <= 126)
+				printf("%c", *(b + i + a));
+			else
+				printf(".");
+		}
+		if (a >= size)
+			continue;
+		printf("\n");
+	}
+	if (size <= 0)
+		printf("\n");
+}
