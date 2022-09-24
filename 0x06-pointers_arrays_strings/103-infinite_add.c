6fe96c5fc38d3be27327dfdf2d558a481d7a16b3
JanetOdemerho <jannyoddie@gmail.com>
2022-09-23 20:04:14 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,42 @@
 #include "main.h"
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
+/**
+ * add_strings - entry point
+ * Description: add the numbers stored in the strings
+ * @n1: first parameter
+ * @n2: second parameter
+ * @r: third parameter
+ * @r_index: fourth parameter
+ * Return: result or 0
+ */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n1; n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--; r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+}
+
 /**
  * infinite_add - entry point
  * Decription: adds two numbers
@@ -13,35 +50,20 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int i, n1l = 0, n2l = 0;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
+	for (i = 0; *(n1 + i); i++)
+		n1l++;
+
+	for (i = 0; *(n2 + i); i++)
+		n2l++;
+
+	if (size_r <= n1l + 1 || size_r <= n2l + 1)
 		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
-	}
-	return (r);
+
+	n1 += n1l - 1;
+	n2 += n2l - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
