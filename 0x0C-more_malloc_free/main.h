53245488dd9c759e220e69bcdcc135b59c77944a
JanetOdemerho <jannyoddie@gmail.com>
2022-10-06 22:23:54 -0700

    Hot fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int convert_digit(char d);
+int str_len(char *s);                                            
+char *array_x(int size);                                         
+char *iterate_zeroes(char *s);                                   
+int convert_digit(char d);                                       
+void mul_string(char *result, char *s, int d, int z);            
+void add_nums(char *r1, char *r2, int r_len);                    
+int main(int argc, char *argv[]); 
 
 #endif
